0e4054419007a1f629dcdba0b6938e13340a271d
Даниил Калугин <kalugin@ruport.ru>
2025-05-22 12:51:12 +0400

    Fix tscongif options, add aliases

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import api from '../../axios';
+import api from '@api/axios';
 
 type Category = string;
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, './src/api'),  // Add this line
+      '@pages': path.resolve(__dirname, './src/pages'),
+    },
+  },
+  server: {
+    port: 3000,
+  }
+});

49e098818692b3abf36a3a29eedaca8f6cd82aa7
Даниил Калугин <kalugin@ruport.ru>
2025-05-22 13:59:34 +0400

    Fix build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@api': path.resolve(__dirname, './src/api'),  // Add this line
+      '@api': path.resolve(__dirname, './src/api'),
       '@pages': path.resolve(__dirname, './src/pages'),
     },
   },

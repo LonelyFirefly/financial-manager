b36e3cb6aa2156e268921ed735f1a61d9d7f7b8d
Даниил Калугин <kalugin@ruport.ru>
2025-05-22 15:04:14 +0400

    Fix webserver config

--- client/src/api/axios/index.ts
+++ client/src/api/axios/index.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const SERVER_URL = import.meta.env.VITE_API_URL;
+// In development, use the full URL. In production, use relative path
+const SERVER_URL = import.meta.env.PROD ? '/api' : import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: SERVER_URL,
   withCredentials: true,
 });
 
-export default api; 
+export default api;

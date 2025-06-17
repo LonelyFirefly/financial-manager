6617b8b8def50633c1533bc5f6185f3dc43eeb25
Даниил Калугин <kalugin@ruport.ru>
2025-06-17 18:12:48 +0400

    Add shadcn data table component

--- client/src/components/layout/investment-table/investment-table.tsx
+++ client/src/components/layout/investment-table/investment-table.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/ui/button';
+import {
+  Table,
+  TableCell,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { useInvestments } from '@/hooks/investments';
 import { InvestmentClientModel } from '@/models/client';
 
@@ -7,10 +16,29 @@ export function InvestmentTable() {
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div>
-      {investments?.map((investment: InvestmentClientModel.InvestmentItem) => (
-        <div key={investment.id}>{investment.tokenInfo.tokenSymbol}</div>
-      ))}
-    </div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Token</TableHead>
+          <TableHead>Quantity</TableHead>
+          <TableHead>Purchase Price</TableHead>
+          <TableHead>Purchase Date</TableHead>
+          <TableHead>Current Price</TableHead>
+          <TableHead>Current Price Change</TableHead>
+          <TableHead>Current Status</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {investments?.map(
+          (investment: InvestmentClientModel.InvestmentItem) => (
+            <TableRow key={investment.id}>
+              <TableCell>{investment.tokenInfo.tokenSymbol}</TableCell>
+              <TableCell>{investment.purchaseInfo.quantity}</TableCell>
+              <TableCell>{investment.purchaseInfo.purchasePrice}</TableCell>
+            </TableRow>
+          )
+        )}
+      </TableBody>
+    </Table>
   );
 }
